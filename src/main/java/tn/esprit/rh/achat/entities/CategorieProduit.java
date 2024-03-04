898007a0107ec745c823eb6140578956b7028f3b
chahabsarah <sarra.chahab@esprit.tn>
2024-03-04 04:29:43 +0100

    modif

--- src/main/java/tn/esprit/rh/achat/entities/CategorieProduit.java
+++ src/main/java/tn/esprit/rh/achat/entities/CategorieProduit.java
@@ -23,7 +23,7 @@ public class CategorieProduit implements Serializable {
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 2L;//1l
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long idCategorieProduit;
